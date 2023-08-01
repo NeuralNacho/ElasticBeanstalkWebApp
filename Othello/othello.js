f019ea99794e823e3c7cf30c3b3c2a10b32d3440
NeuralNacho <anguswhittam@btinternet.com>
2023-08-01 01:46:39 +0100

    Reconstructed the game with rectangular array for board

--- Othello/othello.js
+++ Othello/othello.js
@@ -1,161 +1,147 @@
-const boardSize = 8;
-const emptyCell = '';
+const emptyCell = ''; // Empty cells are empty
 
 let currentPlayer = 'black';
-let board = createBoard();
-let legalMoves = [];
+let board = createBoard(); // Could use class but this is simpler
+let legalMoves = []; // Array of legal moves for current player
 
 function createBoard() {
-  const board = new Array(boardSize * boardSize).fill(emptyCell);
-  board[27] = 'black';
-  board[28] = 'white';
-  board[35] = 'white';
-  board[36] = 'black';
-  return board;
+    // 8x8 array with starting position
+    const board = [];
+    for (let i = 0; i < 8; i++) {
+        board[i] = new Array(8).fill(emptyCell);
+    }
+    board[3][3] = 'white';
+    board[3][4] = 'black';
+    board[4][3] = 'black';
+    board[4][4] = 'white';
+    return board;
 }
 
 function renderBoard() {
-  const boardContainer = document.getElementById('board');
-  boardContainer.innerHTML = '';
+    // For every square on the board display appropriate cell
+    const boardContainer = document.getElementById('board');
+    boardContainer.innerHTML = '';
+    // Above line clears the board each time renderBoard is called
 
-  for (let i = 0; i < boardSize * boardSize; i++) {
-    const cell = document.createElement('div');
-    cell.className = 'cell';
+    for (let row = 0; row < 8; row++) {
+        for (let col = 0; col < 8; col++) {
+            const cell = document.createElement('div');
+            cell.className = 'cell';
+            if (board[row][col] == 'black') {
+                const blackDisc = document.createElement('div');
+                blackDisc.className = 'black-disc';
+                cell.appendChild(blackDisc);
+            }
+            else if (board[row][col] == 'white') {
+                const whiteDisc = document.createElement('div');
+                whiteDisc.className = 'white-disc';
+                cell.appendChild(whiteDisc);
+            }
+            else {
+                cell.addEventListener('click', () => handleMove([row, col]))
+                // Need arrow function for a closure
+                if (legalMoves.some(move => {
+                    return move[0] == row && move[1] == col})) {  // some() method
+                    cell.classList.add('highlighted');
+                }
+            }
 
-    if (board[i] === 'black') {
-      const blackDisc = document.createElement('div');
-      blackDisc.className = 'black-disc';
-      cell.appendChild(blackDisc);
-
-    } else if (board[i] === 'white') {
-      const whiteDisc = document.createElement('div');
-      whiteDisc.className = 'white-disc';
-      cell.appendChild(whiteDisc);
-
-    } else {
-      cell.classList.add('empty');
-      cell.addEventListener('click', () => handleMove(i));
+            boardContainer.appendChild(cell);
+            // Cells added to .board grid row by row
+        }
     }
-
-    // Highlight legal moves
-    if (legalMoves.includes(i)) {
-      cell.classList.add('legal-move');
-    }
-
-    boardContainer.appendChild(cell);
-  }
 }
 
 function handleMove(index) {
-  if (!legalMoves.includes(index)) {
-    return; // Cell is not a legal move
-  }
+    // Called in arrow function on empty cell event listener
+    // If legal move update everything
+    if (!legalMoves.some(move => {
+        return move[0] == index[0] && move[1] == index[1]})) {
+        return;
+    }
+    board[index[0]][index[1]] = currentPlayer;
+    flipTiles(index);  // Important this comes before changing player
+    currentPlayer = currentPlayer === 'black' ? 'white' : 'black';
+    updateLegalMoves();
+    renderBoard();
+}
 
-  board[index] = currentPlayer;
-  flipTiles(index);
-  currentPlayer = currentPlayer === 'black' ? 'white' : 'black';
-  updateLegalMoves();
-  renderBoard();
+function isValidMove(index) {
+    if (board[index[0]][index[1]] != emptyCell) {
+        return false;
+    }
+    const directions = [[-1, -1], [-1, 0], [-1, 1],
+                        [0, -1],           [0, 1],
+                        [1, -1],  [1, 0],  [1, 1] ];
+    for (const direction of directions) {
+        let row = index[0] + direction[0];
+        let col = index[1] + direction[1];
+        let foundOpponentPiece = false;
+        while (row >= 0 && row < 8 && col >= 0 && col < 8) {
+            if (board[row][col] == emptyCell) {
+                break;
+            }
+            if (board[row][col] != currentPlayer) {
+                foundOpponentPiece = true;
+                row += direction[0];
+                col += direction[1];
+                continue;
+            }
+            if (foundOpponentPiece) { // Have  met a disc of currentPlayer
+                return true;
+            }
+            else {
+                break; // If we've not found an oppenent piece
+                // we should still keep checking other directions
+            }
+        }
+    }
+    return false; // If none of the directions work then get false
 }
 
 function updateLegalMoves() {
-  legalMoves = [];
-  for (let i = 0; i < boardSize * boardSize; i++) {
-    if (board[i] === emptyCell && isValidMove(i)) {
-      legalMoves.push(i);
+    // Called by handleMoves
+    legalMoves = [];
+    for (let row = 0; row < 8; row++) {
+        for (let col = 0; col < 8; col++) {
+            if (board[row][col] == emptyCell && isValidMove([row, col])) {
+                legalMoves.push([row, col]);
+            }
+        }
     }
-  }
 }
 
-// Function to check if a move is valid
-function isValidMove(index) {
-    if (board[index] !== emptyCell) {
-      return false; // Cell is not empty, so it's an invalid move
-    }
-  
-    const directions = [
-      [-1, -1], [-1, 0], [-1, 1],
-      [0, -1],           [0, 1],
-      [1, -1],  [1, 0],  [1, 1]
-    ];
-  
-    let isValid = false;
-  
-    for (const [dx, dy] of directions) {
-      let x = index % boardSize;
-      let y = Math.floor(index / boardSize);
-      let foundOpponentPiece = false;
-  
-      // Move in the direction and check if there is an opponent's piece
-      x += dx;
-      y += dy;
-  
-      while (x >= 0 && x < boardSize && y >= 0 && y < boardSize) {
-        const currentIndex = y * boardSize + x;
-  
-        if (board[currentIndex] === emptyCell) {
-          break; // Reached an empty cell, so it's not a valid move in this direction
-        } else if (board[currentIndex] === currentPlayer) {
-          if (foundOpponentPiece) {
-            isValid = true; // Valid move if there's a player's piece after finding an opponent's piece
-            break;
-          } else {
-            break; // No opponent's piece in this direction
-          }
-        } else {
-          foundOpponentPiece = true; // Found an opponent's piece
-        }
-  
-        x += dx;
-        y += dy;
-      }
-    }
-  
-    return isValid;
-  }
-
-// Function to flip tiles based on the current move
 function flipTiles(index) {
-    const directions = [
-      [-1, -1], [-1, 0], [-1, 1],
-      [0, -1],           [0, 1],
-      [1, -1],  [1, 0],  [1, 1]
-    ];
-  
-    for (const [dx, dy] of directions) {
-      let x = index % boardSize;
-      let y = Math.floor(index / boardSize);
-      let foundOpponentPiece = false;
-      const tilesToFlip = [];
-  
-      // Move in the direction and collect opponent's pieces to flip
-      x += dx;
-      y += dy;
-  
-      while (x >= 0 && x < boardSize && y >= 0 && y < boardSize) {
-        const currentIndex = y * boardSize + x;
-  
-        if (board[currentIndex] === emptyCell) {
-          break; // Reached an empty cell, so no opponent's pieces to flip in this direction
-        } else if (board[currentIndex] === currentPlayer) {
-          if (foundOpponentPiece) {
-            // Flip opponent's pieces between the current cell and the original move
-            for (const tileIndex of tilesToFlip) {
-              board[tileIndex] = currentPlayer;
+    // Called by handleMoves
+    const directions = [[-1, -1], [-1, 0], [-1, 1],
+                        [0, -1],           [0, 1],
+                        [1, -1],  [1, 0],  [1, 1] ];
+    for (const direction of directions) {
+        const tilesToFlip = [];
+        let row = index[0] + direction[0];
+        let col = index[1] + direction[1];
+        while (row >= 0 && row < 8 && col >= 0 && col < 8) {
+            if (board[row][col] == emptyCell) {
+                break;
             }
-          }
-          break;
-        } else {
-          foundOpponentPiece = true; // Found an opponent's piece
-          tilesToFlip.push(currentIndex);
+            if (board[row][col] != currentPlayer) {
+                tilesToFlip.push([row, col]);
+                row += direction[0];
+                col += direction[1];
+                continue;
+            }
+            if (board[row][col] == currentPlayer) {
+                console.log(tilesToFlip)
+                for (index of tilesToFlip) {
+                    console.log(index)
+                    board[index[0]][index[1]] = currentPlayer;
+                }
+                break
+            }
         }
-  
-        x += dx;
-        y += dy;
-      }
     }
-  }
-  
+}
 
+// Initialise the game:
 updateLegalMoves();
 renderBoard();
